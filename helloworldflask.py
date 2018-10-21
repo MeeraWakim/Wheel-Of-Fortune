7760e02a2bf54a55bbce9bb153dc14c48af9c056
ZachPope531 <zachary.pope531@gmail.com>
2018-10-21 00:06:18 -0500

    Finished transferring the file from Vue to Flask

--- helloworldflask.py
+++ helloworldflask.py
@@ -1,11 +1,35 @@
-# -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+import sys, os
+
 app = Flask(__name__)
 
-@app.route('/')
+POST_FILE = None
+
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+cors = CORS(app, resources={r"/foo": {"origins": "http://localhost:8080"}})
+
+
+@app.route('/uploads/data', methods=['POST'])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def hello_world():
-    return 'Hello, World!'
+    global POST_FILE
+    if request.method == "POST":
+        POST_FILE = request.files['file'].read()
+        output_file = open("output.docx", 'wb')
+        output_file.write(POST_FILE)
+        output_file.close()
+        POST_FILE = None
+        return 'Good'
+    else:
+        print "Bad method:", request.method
+        return 'Bad'
 
 @app.route('/this-is-so-easy')
 def ezpz():
-    return 'ファックユーダジャエンゴ'
+    return "Screw Django"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=8081)
--- src/file-upload.service.js
+++ src/file-upload.service.js
@@ -1,15 +1,10 @@
 import * as axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:8080';
+const BASE_URL = 'http://127.0.0.1:8081';
 
 function upload(formData) {
-    const url = `${BASE_URL}/files/upload`;
+    const url = `${BASE_URL}/uploads/data`;
     return axios.post(url, formData)
-        /*// get data
-        .then(x => x.data)
-        // add url field
-        .then(x => x.map(img => Object.assign({},
-            img, { url: `${BASE_URL}/uploads/${img.id}` })));*/
 }
 
 export { upload }
