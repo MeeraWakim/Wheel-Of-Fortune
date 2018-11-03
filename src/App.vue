7720f59e4bac9c428cb4d924d02ff9fcc37e4b10
Clarice <clarice_norton@utexas.edu>
2018-11-02 20:04:01 -0500

    Added more to the layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    <!-- Toolbars -->
     <v-toolbar>
       <v-toolbar-side-icon></v-toolbar-side-icon>
       <v-toolbar-title>Title</v-toolbar-title>
@@ -10,6 +11,37 @@
         <v-btn flat>Link Three</v-btn>
       </v-toolbar-items>
     </v-toolbar>
+
+    <!-- Main container -->
+    <v-container fluid fill-height>
+      <v-layout justify-center align-center>
+        <v-flex shrink>
+          <v-tooltip right>
+            <v-btn
+              slot="activator"
+              :href="source"
+              icon
+              large
+              target="_blank"
+            >
+              <v-icon large>code</v-icon>
+            </v-btn>
+            <span>Source</span>
+          </v-tooltip>
+          <v-tooltip right>
+            <v-btn slot="activator" icon large href="https://codepen.io/johnjleider/pen/qxQWda" target="_blank">
+              <v-icon large>mdi-codepen</v-icon>
+            </v-btn>
+            <span>Codepen</span>
+          </v-tooltip>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    
+    <!--Footer-->
+    <v-footer app fixed>
+      <span>A Summer Knight Project</span>
+    </v-footer>
   </v-app>
 </template>
 
