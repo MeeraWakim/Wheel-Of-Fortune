bbddab01c205e1b64da0b4f2912e330c5fa3b222
Clarice <clarice_norton@utexas.edu>
2018-11-02 20:38:00 -0500

    More layout changes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-app>
-    <!-- Toolbars -->
-    <v-toolbar>
+  <v-app dark>
+    <!-- Toolbar -->
+    <v-toolbar class="light-green darken-3">
       <v-toolbar-side-icon></v-toolbar-side-icon>
-      <v-toolbar-title>Title</v-toolbar-title>
+      <v-toolbar-title>Ring of Fortune</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn flat>Link One</v-btn>
@@ -15,32 +15,13 @@
     <!-- Main container -->
     <v-container fluid fill-height>
       <v-layout justify-center align-center>
-        <v-flex shrink>
-          <v-tooltip right>
-            <v-btn
-              slot="activator"
-              :href="source"
-              icon
-              large
-              target="_blank"
-            >
-              <v-icon large>code</v-icon>
-            </v-btn>
-            <span>Source</span>
-          </v-tooltip>
-          <v-tooltip right>
-            <v-btn slot="activator" icon large href="https://codepen.io/johnjleider/pen/qxQWda" target="_blank">
-              <v-icon large>mdi-codepen</v-icon>
-            </v-btn>
-            <span>Codepen</span>
-          </v-tooltip>
-        </v-flex>
+      <!--Stuff goes in here (Wheel)-->
       </v-layout>
     </v-container>
     
     <!--Footer-->
     <v-footer app fixed>
-      <span>A Summer Knight Project</span>
+      <span class="light-green darken-3">A Summer Knight Project</span>
     </v-footer>
   </v-app>
 </template>
