b50e1b30ebeca36e600ea48d9971975958d7152b
Clarice <clarice_norton@utexas.edu>
2018-11-03 13:42:22 -0500

    more layout stuff

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <template>
   <v-app dark>
     <!-- Toolbar -->
-    <v-toolbar class="light-green darken-3">
-      <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar dark color="primary">
       <v-toolbar-title>Ring of Fortune</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat>Link One</v-btn>
-        <v-btn flat>Link Two</v-btn>
-        <v-btn flat>Link Three</v-btn>
-      </v-toolbar-items>
     </v-toolbar>
 
     <!-- Main container -->
--- src/main.js
+++ src/main.js
@@ -7,7 +7,18 @@ import 'vuetify/dist/vuetify.min.css'
 const VueScrollTo = require('vue-scrollto')
 
 Vue.use(VueScrollTo)
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  theme: {
+    "primary": "#455a64",
+    "secondary": "#5e35b1",
+    "accent": "#aa00ff",
+    "error": "#c62828",
+    "info": "#78909c",
+    "success": "#4caf50",
+    "warning": "#fbc02d"
+  }
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
