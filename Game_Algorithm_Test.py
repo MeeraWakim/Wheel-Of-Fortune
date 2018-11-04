7af6f42822f5981a9a86f839b9cf024f8a2ebfd2
Dennis <43227874+ThisIsADennis@users.noreply.github.com>
2018-11-03 23:56:52 -0500

    Add files via upload

--- Game_Algorithm_Test.py
+++ Game_Algorithm_Test.py
@@ -3,7 +3,7 @@ import Game_Algorithm
 
 class TestGameAlgorithm():
     def test_showWord(self):
-        self.AssertEqual(showWord("Testing phrase",["g", "t", "i", "p", "n", "e"]), "T __  __ ting p __ __ __ __ e")
+        self.AssertEqual(showWord("Testing phrase",["g", "t", "i", "p", "n", "e"]), "T __  __ ting p __  __  __  __ e")
 
 if __name__ == '__main__':
 	unittest.main()
