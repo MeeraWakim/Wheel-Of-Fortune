957e8754268c4021f3cc8e7d1edf6e1b076c05a7
Dennis <43227874+ThisIsADennis@users.noreply.github.com>
2018-11-03 23:29:21 -0500

    Add files via upload

--- Game_Algorithm.py
+++ Game_Algorithm.py
@@ -1,4 +1,3 @@
-
 import random
 import sys
 
@@ -8,53 +7,53 @@ class player:
 		self.name = name
 		self.score = 0
 
-	def spin(self,wheel_dict):
-		num = random.randint(1,281)
+	def spin(self,wheel_dict):  # Chooses a "weighted" random integer that then decides the money value
+		num = random.randint(1,281)  # Random integer from 1 - 281, inclusive
 		value = 0
-		if num <= 50:
+		if num <= 50:  # 1 - 50
 			value = wheel_dict[1]
-		elif num <= 100:
+		elif num <= 100:  # 51 - 100
 			value = wheel_dict[2]
-		elif num <= 150:
+		elif num <= 150:  # 101 - 150
 			value = wheel_dict[3]
-		elif num <= 200:
+		elif num <= 200:  # 151 - 200
 			value = wheel_dict[4]
-		elif num <= 250:
+		elif num <= 250:  # 201 - 250
 			value = wheel_dict[5]
-		elif num <= 275:
+		elif num <= 275:  # 251 - 275
 			value = wheel_dict[10]
-		elif num <= 280:
+		elif num <= 280:  # 275 - 280
 			value = wheel_dict[100]
-		else:
+		else:  # 281
 			value = wheel_dict[1000]
 		return(value)
 
-def showWord(phrase,guessed):
+def showWord(phrase,guessed):  # Displays the phrase with the letters that haven't been guessed yet removed
 
 	print("Word: ")
 	for i in range(len(phrase)):
-		if phrase[i].lower() not in guessed and phrase[i].upper() not in guessed:
-			if phrase[i] == " ":
+		if phrase[i].lower() not in guessed and phrase[i].upper() not in guessed:  # If the guessed letters are not in the list containing the list of correctly guessed letters
+			if phrase[i] == " ":  # Whitespace
 				print("  ",end = " ")
 			else:
 				print("__", end = " ")
 		else:
-			print(phrase[i], end = " ")
+			print(phrase[i], end = " ")  # Print the letter from the original phrase
 
-def main():
+def main():  # Main program
 
-	wheel_dict = {1 : 1000, 2 : 2000, 3 : 3000, 4 : 4000, 5 : 5000, 10 : 10000, 100 : 100000, 1000: 1000000}
+	wheel_dict = {1 : 1000, 2 : 2000, 3 : 3000, 4 : 4000, 5 : 5000, 10 : 10000, 100 : 100000, 1000: 1000000}  # Possible prizes
 
 	phraseFile = open("Phrases.txt", "r")
 	phrases = []
 	for line in phraseFile:
-		if line.find("\n") != -1:
+		if line.find("\n") != -1:  # Not the last phrase in the document
 			phrases.append(line[:-1])
-		else:
+		else:  # Last phrase in document
 			phrases.append(line)
 
 	num_players = input("Enter number of players: ")
-	while True:
+	while True:  # Checking if the user inputted a positive integer
 		if num_players.find(".") >= 0 or num_players.find("-") >= 0:
 			num_players = input("You didn't type in a positive integer. Please try again: ")
 		else:
@@ -74,10 +73,10 @@ def main():
 
 	for i in range(0,num_players):
 		player_name = input("Player " + str(i + 1) + ", enter your name: ")
-		players.append(player(player_name))
+		players.append(player(player_name))  # Appends created player object to players
 
 	num_phrases = input("\nThere are " + str(len(phrases)) + " phrases to choose from. Enter the number of phrases you want to guess: ")
-	while True:
+	while True:  # Checking if the user inputted a positive integer
 		if num_phrases.find(".") >= 0 or num_phrases.find("-") >= 0:
 			num_phrases = input("You didn't type in a positive integer. Please try again: ")
 		else:
@@ -94,17 +93,17 @@ def main():
 				num_phrases = input("You didn't type in a positive integer. Please try again: ")
 	print("")
 
-	for gameNum in range(num_phrases):
+	for gameNum in range(num_phrases):  # Looping over the chosen phrases
 
-		notWin = True
-		turn = random.randint(1,num_players - 1)
+		notGuessed = True  # No one completed the phrase yet
+		turn = random.randint(1,num_players - 1)  # Chooses a random player to start off
 		num_guessedLetters = 0
 		phrase = phrases[gameNum]
 		guessed = []
 		print("Phrase #" + str(gameNum + 1) + " out of " + str(num_phrases))
 		print("--------------------------")
 
-		while notWin:
+		while notGuessed:  # While the phrase hasn't been guessed yet
 
 			print("\nIt is " + players[turn].name + "'s turn.")
 			dummy = input(" ~ Press Enter to spin the wheel ~ \n")
@@ -113,7 +112,7 @@ def main():
 			showWord(phrase,guessed)
 			print("\n" + players[turn].name + ", for $" + str(value) + ", guess a letter.")
 			guess = input("Letter: ")
-			while True:
+			while True:  # Checking if the user entered a single letter (uppercase or lowercase)
 				if guess.isalpha():
 					if len(guess) == 1:
 						break
@@ -123,24 +122,25 @@ def main():
 					guess = input("You didn't type in a letter. Please try again: ")
 			correct = True
 
-			while correct:
-				if (guess.lower() in phrase) or (guess.upper() in phrase):
-					if guess in guessed:
+			while correct:  # While the player is correct
+				if (guess.lower() in phrase) or (guess.upper() in phrase):  # If the letter (upper or lowercase) is in the phrase
+					if guess in guessed:  # If the letter has already been guessed
 						print("That letter has already been guessed.")
 						correct = False
 					else:
-						players[turn].score += value
-						guessed.append(guess)
+						players[turn].score += value  # Player gets prize
+						guessed.append(guess)  # Letter is now guessed
 						num_guessedLetters += 1
-						if len("".join(set(phrase.replace(" ", "")))) == num_guessedLetters:
+						if len("".join(set(phrase.replace(" ", "")))) == num_guessedLetters:  # If the number of guessed letters equals the number of UNIQUE letters in the phrase
+						# Quit the two while loops (one for correct, one for notGuessed)
 							print("Correct! Your now have $" + str(players[turn].score) + ".")
 							print("\nThe phrase has been revealed by " + str(players[turn].name) + "!")
 							print("The phrase was: " + phrase + "\n")
-							if gameNum != num_phrases - 1:
+							if gameNum != num_phrases - 1:  # If it isn't the last phrase
 								print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
-							notWin = False
+							notGuessed = False
 							correct = False
-						else:
+						else:  # If there are still letters to be guessed
 							print("Correct! Your now have $" + str(players[turn].score) + ". Spin again.")
 							dummy = input(" ~ Press Enter to spin the wheel ~ \n")
 							value = players[turn].spin(wheel_dict)
@@ -155,28 +155,28 @@ def main():
 										guess = input("You didn't type in a single letter. Please try again: ")
 								else:
 									guess = input("You didn't type in a letter. Please try again: ")
-				else:
+				else:  # Player guessed incorrectly
 					print("Incorrect!")
 					correct = False
 
-			if turn < (num_players - 1):
-				turn += 1
-			else:
-				turn = 0
+			if turn < (num_players - 1):  # If the last player hasn't been reached yet
+				turn += 1  # Next player
+			else:  # The last player has been reached
+				turn = 0  # Back to player 1
 	print("The game has concluded!")
 	winningScore = 0
-	for the_player in players:
+	for the_player in players:  # Calculating the winning score
 		print(the_player.name + " ended with $" + str(the_player.score) + "!")
 		if winningScore < the_player.score:
 			winningScore = the_player.score
 
-	winners = []
+	winners = []  # In case of ties
 	for the_player in players:
 		if winningScore == the_player.score:
 			winners.append(the_player.name)
-	if len(winners) == 1:
+	if len(winners) == 1:  # Only one winner
 		print("\nThe winner is " + winners[0] + " with an ending amount of $" + str(winningScore) + "!")
-	else:
+	else:  # There was a tie!
 		print("\nThere is a tie! The winners are:")
 		for player_name in winners:
 			print(player_name)
